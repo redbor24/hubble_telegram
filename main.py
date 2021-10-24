5f7ac3f18df9829875d6a41e6d757518aef7b4ef
redbor <redbor24@gmail.com>
2021-10-24 14:16:56 +0300

    Скачивание картинки вынесено в функцию download_image(url, filename). Скорректирован README.md в соответствии с кодом

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ from pathlib import Path
 
 IMAGES_FOLDER = 'images'
 
-headers = {
-    'User-Agent': 'curl',
-    'Accept-Language': 'ru-RU'
-}
 
-filename = 'hubble.jpeg'
-url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+def download_image(url, filename):
+    headers = {
+        'User-Agent': 'curl',
+        'Accept-Language': 'ru-RU'
+    }
 
-response = requests.get(url, headers=headers)
-response.raise_for_status()
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
 
-images_directory = Path.cwd() / IMAGES_FOLDER
-if not Path.is_dir(images_directory):
-    Path.mkdir(images_directory)
+    images_directory = Path.cwd() / IMAGES_FOLDER
+    if not Path.is_dir(images_directory):
+        Path.mkdir(images_directory)
 
-with open(Path(images_directory) / filename, 'wb') as file:
-    file.write(response.content)
+    with open(Path(images_directory) / filename, 'wb') as file:
+        file.write(response.content)
+
+
+if __name__ == '__main__':
+    download_image(
+        'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg',
+        'hubble.jpeg')
